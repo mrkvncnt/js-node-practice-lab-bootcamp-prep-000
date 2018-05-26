e3f75dd60b1f21d1913379b4e071afb04ecc643d
Mark Rodriguez <mrkxvncnt@gmail.com>
2018-05-26 22:52:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 
 var name = "Janet"
 var height = "74"
+var growth = "100"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log(parseInt(height) + (growth))
